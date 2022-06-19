7e84600ed03dfd5aba70e17860a67bde389fbb98
o7lili <o7lilip@gmail.com>
2022-06-19 13:02:26 -0400

    save high score feature added

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,29 +4,26 @@
 //  * Defeat each enemy-robot
 // "LOSE" - Player robot's health is zero or less
 
-<<<<<<< HEAD
-/* GAME FUNCTIONS */
-
 // function to generate a random numeric value
 var randomNumber = function(min, max) {
-    var value = Math.floor(Math.random() * (max - min + 1) + min);
+    var value = Math.floor(Math.random() * (max - min) + min);
 
     return value;
 };
 
-// fight function (now with parameter for enemy's object holding name, health, and attack values)
-=======
 //fight or skip function
 var fightOrSkip = function() {
     // ask player if they'd like to fight or skip using fightOrSkip funtion
     var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
 
-    // Conditional Recursive Function Call
+    // validate prompt answer
     if (promptFight === "" || promptFight === null) {
         window.alert("You need to provide a valid answer! Please try again.");
+        // use return to call it again and stop the rest of this function from running
         return fightOrSkip();
     }
 
+    //convert promptFight to all lowercase so we an check with less options
     promptFight = promptFight.toLowerCase();
 
     // if player picks "skip" confirm and then stop the loop
@@ -45,11 +42,10 @@ var fightOrSkip = function() {
 
         }
     }
-return false;
+    return false;
 }
 
-// fight function (now with parameter for enemy's name)
->>>>>>> develop
+// fight function (now with parameter for enemy's object holding name, health, and attack values)
 var fight = function(enemy) {
     // keep track of who goes first
     var isPlayerTurn = true;
@@ -59,7 +55,6 @@ var fight = function(enemy) {
         isPlayerTurn = false;
     }
 
-    // repeat and execute as long as the enemy-robot is alive
     while(playerInfo.health > 0 && enemy.health > 0) {
         if (isPlayerTurn) {
             // ask player if they'd like to fight or skip using fightOrSkip function
@@ -68,7 +63,6 @@ var fight = function(enemy) {
                 break;
             }
 
-            // generate random damage value based on player's attack power
             var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
 
             // remove enemy's health by subtracting the amount we set in the damage variable
@@ -134,6 +128,9 @@ var startGame = function() {
 
     // fight each enemy-robot by looping over them and fighting them one at a time
     for(var i = 0; i < enemyInfo.length; i++) {
+        // check player stats
+        console.log(playerInfo);
+
         // if player is still alive, keep fighting
         if (playerInfo.health > 0) {
             // let player know what round they are in, array starts at 0 so 1 needs to be added
@@ -145,14 +142,9 @@ var startGame = function() {
             // set health for picked enemy  
             pickedEnemyObj.health = randomNumber(40, 60);
 
-<<<<<<< HEAD
-            // use debugger to pause script from running and check what's going on at that moment in the code
-            // debugger;
+            console.log(pickedEnemyObj);
 
-            // pass the pickedEnemyObj object variable's value into the fight function, where it will assume the value of the enemy parameter
-=======
             // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemy.name parameter
->>>>>>> develop
             fight(pickedEnemyObj);
 
             // if player is still alive and we're not at the last enemy in the array
@@ -181,11 +173,20 @@ var startGame = function() {
 var endGame = function() {
     window.alert("The game has now ended. Let's see how you did!");
 
-    // if player is still alive, player wins!
-    if (playerInfo.health > 0) {
-        window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + ".");
-    }   else {
-        window.alert("You've lost your robot in battle.");
+    //check localStorage for high score, if it's not there, use 0
+    var highScore = localStorage.getItem("highscore");
+    if (highScore === null) {
+        highScore = 0;
+    }
+    // if player has more money than the high score, player has new high score!
+    if (playerInfo.money > highScore) {
+        localStorage.setItem("highscore", playerInfo.money);
+        localStorage.setItem("name", playerInfo.name);
+
+        alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
+    }
+    else {
+        alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
     }
 
     // ask player if they'd like to play again
@@ -193,7 +194,8 @@ var endGame = function() {
 
     if (playAgainConfirm) {
         startGame();
-    }   else {
+    }   
+    else {
         window.alert("Thank you for playing Robot Gladiators! Come back soon!");
     }
 };
@@ -205,7 +207,9 @@ var shop = function() {
         "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE."
     );
 
+    // convert answer from prompt to an actual number
     shopOptionPrompt = parseInt(shopOptionPrompt);
+
     // use switch to carry out action
     switch (shopOptionPrompt) {
         case 1:
@@ -224,13 +228,8 @@ var shop = function() {
     }
 };
 
-/* END GAME FUNCTIONS */
-
 /* GAME INFORMATION / VARIABLES */
 
-<<<<<<< HEAD
-//player information
-=======
 //function to set name
 var getPlayerName = function() {
     var name = "";
@@ -243,8 +242,9 @@ var getPlayerName = function() {
     return name;
 };
 
+/* END GAME FUNCTIONS */
+
 /* GAME INFORMATION / VARIABLES */
->>>>>>> develop
 var playerInfo = {
     name: getPlayerName(),
     health: 100,
@@ -292,11 +292,6 @@ var enemyInfo = [
         attack: randomNumber(10, 14)
     }
 ];
-
-console.log(enemyInfo);
-console.log(enemyInfo[0]);
-console.log(enemyInfo[0].name);
-console.log(enemyInfo[0]['attack']);
 
 /* END GAME INFORMATION / VARIABLES */
 
